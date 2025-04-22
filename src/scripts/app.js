fba4d846dc60ee293db58f606cf7330f8bde82d0
Jarne <descheerderjarne@gmail.com>
2025-04-22 15:41:57 +0200

    avancement

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,34 @@
-console.info('Hello world');
+"use strict";
+
+/*Code repris des codes aides fait avec Monsieur Thronte*/
+var btnTheme = document.querySelector(".btn--darkMode");
+
+btnTheme.addEventListener("click", themeSelect);
+
+function themeSelect(){
+    var currentMode = document.body.getAttribute("data-theme");
+    var systemThemeDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if((!currentMode && systemThemeDark) || currentMode == "dark"){
+        document.body.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    } else{
+        document.body.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
+}
+
+var activeTheme = localStorage.getItem("theme");
+if(activeTheme){
+    document.body.setAttribute("data-theme", activeTheme);
+}
+
+/* menu */
+
+var menuBtn = document.querySelector(".menu__btn");
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    var menu = document.querySelector(".menu");
+    menu.classList.toggle("menu--open");
+}
