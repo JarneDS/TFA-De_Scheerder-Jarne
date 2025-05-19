ea5542c55f7a573e2a1134d8163bb138052f6ecd
Jarne <descheerderjarne@gmail.com>
2025-05-19 16:13:10 +0200

    modifs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -108,3 +108,17 @@ function closeCredits() {
     creditsBlock.classList.remove("credits__section--active");
     document.body.style.overflow = "";
 }
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            navLinks.forEach(link => link.classList.remove("active"));
+            document.querySelector(`nav a[href="#${entry.target.id}"]`).classList.add("active");
+        }
+    });
+}, { threshold: 0.5 });
+
+sections.forEach(section => observer.observe(section));
